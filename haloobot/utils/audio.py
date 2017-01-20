538f52aaae28bd4d4624d042098b36432b54530c
laitalaj <julius.laitala@helsinki.fi>
2017-01-20 12:43:15 +0200

    Seems like the mp3 step is necessary after all

--- haloobot/utils/audio.py
+++ haloobot/utils/audio.py
@@ -12,9 +12,9 @@ def text_to_ogg(text, lang = 'en', ttsid = 0):
     tts = gTTS((text), lang)
     #mp3path = path.abspath(path.join('tts', 'tts%s.mp3' % ttsid))
     oggpath = path.abspath(path.join('tts', 'tts%s.ogg' % ttsid))
-    #frommp3 = subprocess.Popen(['mpg123', '-w', '-', str(mp3path)], stdout=subprocess.PIPE)
-    toogg = subprocess.Popen(['oggenc', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    tts.write_to_fp(toogg.stdin)
+    frommp3 = subprocess.Popen(['mpg123', '-s', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    toogg = subprocess.Popen(['oggenc', '-'], stdin=frommp3.stdout, stdout=subprocess.PIPE)
+    tts.write_to_fp(frommp3.stdin)
     outfile = open(oggpath, 'wb')
     while True:
         data = toogg.stdout.read(1024 * 100)
