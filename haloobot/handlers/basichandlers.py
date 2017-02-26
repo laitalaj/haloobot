d5ff8785a87a95d26588da37e647c1471b4bb020
laitalaj <julius.laitala@helsinki.fi>
2017-02-27 00:22:22 +0200

    Made sticker replies depend on sticker emojis

--- haloobot/handlers/basichandlers.py
+++ haloobot/handlers/basichandlers.py
@@ -77,8 +77,20 @@ class StickerHandler(Handler):
         sticker = msg['sticker']
         if self.tables['stickers'].insert_ignore({'file_id': sticker['file_id']}, ['file_id']):
             print('Added new sticker ...%s!' % sticker['file_id'][-8:])
+        if 'emoji' in sticker: # Keep that old db working and the emoji up to date
+            self.tables['stickers'].update({'file_id': sticker['file_id'], 'emoji': sticker['emoji']}, ['file_id'])
         if random.random() < self.settings['trigger']:
-            to_send = self.tables['db'].query('SELECT * FROM stickers ORDER BY RANDOM() LIMIT 1').next()
+            to_send = None
+            for emoji in sticker['emoji']:
+                if emoji in '();/*-': # Should hinder any SQL injetions in case someone manages to feed non-emojis through the api
+                    continue
+                try:
+                    to_send = self.tables['db'].query('SELECT * FROM stickers WHERE emoji LIKE %{}% ORDER BY RANDOM() LIMIT 1'.format(emoji)).next()
+                except StopIteration:
+                    pass
+            if to_send == None:
+                to_send = self.tables['db'].query('SELECT * FROM stickers ORDER BY RANDOM() LIMIT 1').next()
+                print('Didn\'t find any stickers with matching emoji >:')
             await self.send_sticker(chat_id, to_send['file_id'])
             print('Sent sticker ...%s as a reply to ...%s' % (to_send['file_id'][-8:], sticker['file_id'][-8:]))
             speakercounters.update_speaker_triggers(msg, self.tables)
