100a07864867395d28be2307dc196d3bc5986597
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2018-03-29 10:04:50 +0300

    updated excuse

--- haloobot/utils/excuse.py
+++ haloobot/utils/excuse.py
@@ -6,6 +6,6 @@ def getexcuse():
         urllib.request.urlopen('https://ohjelmointitekosyyt.fi/')
         .read()
         .decode('UTF-8')
-        .split('<p><a href=".">')[1]
+        .split('<p class="excuse"><a href=".">')[1]
         .split('</a></p>')[0]
     )
