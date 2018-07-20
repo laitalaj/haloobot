081f26a0899f978eacfcaae520ab7689b111f890
Julius Laitala <jiilaitala@gmail.com>
2018-07-20 20:11:15 +0300

    Get rid of RE import and repeated code

--- haloobot/utils/fingerpori.py
+++ haloobot/utils/fingerpori.py
@@ -1,17 +1,20 @@
 import aiohttp
-import re
 from bs4 import BeautifulSoup
 
-async def get_newest_fingerpori():
+async def _do_get_fingerpori(url, get_comic_url):
     async with aiohttp.ClientSession() as http:
-        async with http.get('https://www.hs.fi/fingerpori/') as r:
+        async with http.get(url) as r:
             soup = BeautifulSoup(await r.text(), 'html.parser')
-            comic_url = 'https:' + soup.find(class_='cartoon').noscript.img.get('src')
-            return comic_url
+            return get_comic_url(soup)
+
+async def get_newest_fingerpori():
+    return await _do_get_fingerpori(
+        'https://www.hs.fi/fingerpori/',
+        lambda soup: 'https:' + soup.find(class_='cartoon').noscript.img.get('src')
+    )
 
 async def get_newest_fingerpori_b():
-    async with aiohttp.ClientSession() as http:
-        async with http.get('http://www.kaleva.fi/fingerpori/') as r:
-            soup = BeautifulSoup(await r.text(), 'html.parser')
-            comic_url = soup.find(class_='comics__strip__image').get('src')
-            return comic_url
+    return await _do_get_fingerpori(
+        'http://www.kaleva.fi/fingerpori/',
+        lambda soup: soup.find(class_='comics__strip__image').get('src')
+    )
